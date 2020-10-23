1e714c80428e546c70648a832c669b0295bce81a
Данила Кондратенко <dan.kondratenko2013@ya.ru>
2020-10-23 17:14:07 +0600

    Добавление фона и исправление ошибок

--- core/src/ru/danila/koloboks/GameObject.java
+++ core/src/ru/danila/koloboks/GameObject.java
@@ -6,7 +6,11 @@ import com.badlogic.gdx.utils.Disposable;
 
 public class Kolobok implements Disposable {
     final String TEXTURE_NAME = "kolobok.png";
-    float x = 0, y = 0, dx, dy;
+    public static final int IMG_WIDTH = 16;
+    public static final int IMG_HEIGHT = 16;
+
+    float x = 0, y = 0;
+    private float dx, dy;
     Texture img;
 
     Kolobok(float x, float y, float dx, float dy) {
@@ -21,10 +25,14 @@ public class Kolobok implements Disposable {
     public void move() {
         this.x += this.dx;
         this.y += this.dy;
+        if ((this.x >= Screen.SCR_WIDTH-IMG_WIDTH) || (this.x <= 0))
+            this.dx *= -1;
+        if ((this.y >= Screen.SCR_HEIGHT-IMG_HEIGHT) || (this.y <= 0))
+            this.dy *= -1;
     }
 
     public void draw(SpriteBatch batch) {
-        batch.draw(img, x, y);
+        batch.draw(img, x, y, IMG_WIDTH, IMG_HEIGHT);
     }
 
     @Override
